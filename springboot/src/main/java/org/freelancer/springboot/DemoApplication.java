53c32fc8fdd25ffe60dde7328e07754de51dffe2
liqiang.fang <liqiang.fang@qunar.com>
2018-03-21 17:43:27 -0700

    init

---
+++ springboot/src/main/java/org/freelancer/springboot/DemoApplication.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2016 Qunar.com. All Rights Reserved.
+ */
+package org.freelancer.springboot;
+
+/**
+ * @author abel created on 2018/3/20 下午4:53
+ * @version $Id$
+ */
+
+public class DemoApplication {
+}
